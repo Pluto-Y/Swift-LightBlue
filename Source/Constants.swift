9418f9e95ce851fd1fd77cec0e760fa3c1fb84a4
Pluto Y <kuaileainits@163.com>
2016-04-20 01:12:56 +0800

    Add Constants for Swift-LightBlue

---
+++ Source/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Swift-LightBlue
+//
+//  Created by Pluto Y on 4/20/16.
+//  Copyright © 2016 Pluto-y. All rights reserved.
+//
+
+import Foundation
+
+enum PeripheralNotificationKeys : String { // The notification name of peripheral
+    case DisconnectNotif = "disconnectNotif" // Disconnect notification name
+    case CharacteristicNotif = "characteristicNotif" // Characteristic discover notification name 
+}
