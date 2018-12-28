6db77c832591f2d5f200c10572aa081a24a97efd
Pluto-Y <kuaileainits@163.com>
2018-12-28 11:03:32 +0800

    Add new store for VirtualPeripheral

---
+++ Swift-LightBlue/VirtualPeripheralStore.swift
@@ -0,0 +1,13 @@
+//
+//  VirtualPeripheralStore.swift
+//  Swift-LightBlue
+//
+//  Created by Longhua Zhang on 2018/12/27.
+//  Copyright © 2018 Pluto-y. All rights reserved.
+//
+
+import Foundation
+
+class VirtualPeripheralStore: NSObject {
+    
+}
