2d13720c8bc3c3263dcabac617f8c8d03e8f789e
Pluto-Y <kuaileainits@163.com>
2018-12-30 05:18:24 +0800

    feature: finish the simplest way of caching 'VirtualPeripheral'

--- Swift-LightBlue/VirtualPeripheralStore.swift
+++ Swift-LightBlue/VirtualPeripheralStore.swift
@@ -11,8 +11,36 @@ import Foundation
 class VirtualPeripheralStore: NSObject {
     
     static let shared = VirtualPeripheralStore()
+    let virtualPeripheralsKey = "virtualPeripheralUserDefaultKey"
+    
+    var cachedVirtualPeripheral: [VirtualPeripheral]
+    
+    override init() {
+        if let data = UserDefaults.standard.object(forKey: virtualPeripheralsKey) as? Data, let array = try? JSONDecoder().decode(Array<VirtualPeripheral>.self, from: data) {
+            cachedVirtualPeripheral = array
+        } else {
+            cachedVirtualPeripheral = []
+        }
+    }
     
     func add(virtualPeripheral: VirtualPeripheral) {
-        
+        cachedVirtualPeripheral.append(virtualPeripheral)
+        syncVirtualPeriperals()
+    }
+    
+    func remove(virtualPeripheral: VirtualPeripheral) {
+        guard let index = cachedVirtualPeripheral.firstIndex(of: virtualPeripheral) else {
+            return
+        }
+        cachedVirtualPeripheral.remove(at: index)
+        syncVirtualPeriperals()
+    }
+    
+    private func syncVirtualPeriperals() {
+        guard let encodedData = try? JSONEncoder().encode(cachedVirtualPeripheral) else {
+            return
+        }
+        UserDefaults.standard.set(encodedData, forKey: virtualPeripheralsKey)
+        UserDefaults.standard.synchronize()
     }
 }
