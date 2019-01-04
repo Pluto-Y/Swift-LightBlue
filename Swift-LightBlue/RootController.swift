9b0b3ec644030f6ddf3ff8e053f124a33a56e6aa
Pluto-Y <kuaileainits@163.com>
2019-01-04 17:22:43 +0800

    feature: Make the toolbar in bottom at the navigationController level, not at MainController level

--- Swift-LightBlue/RootController.swift
+++ Swift-LightBlue/RootController.swift
@@ -12,8 +12,39 @@ class RootController : UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initAll()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.setToolbarHidden(false, animated: false)
+        
+        let infoBarButtonItem = UIBarButtonItem(title: "Info", style: .plain, target: self, action: #selector(didInfoClick(_:)))
+        let flexbleSpaceBarButtomItem1 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let aboutBarButtomItem = UIBarButtonItem(title: "About", style: .plain, target: self, action: #selector(didAboutClick(_:)))
+        let flexbleSpaceBarButtomItem2 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let logBarButtomItem = UIBarButtonItem(title: "Log", style: .plain, target: self, action: #selector(didLogClick(_:)))
+        let toolbarItems = [infoBarButtonItem, flexbleSpaceBarButtomItem1, aboutBarButtomItem, flexbleSpaceBarButtomItem2, logBarButtomItem]
+        self.toolbar.setItems(toolbarItems, animated: false)
+    }
+    
+    private func initAll() {
         self.navigationBar.isTranslucent = false
         self.view.backgroundColor = .white
+        self.setToolbarHidden(false, animated: false)
+    }
+    
+    @objc private func didInfoClick(_ sender: Any) {
+        present(UINavigationController(rootViewController: InfoViewController()), animated: true, completion: nil)
+    }
+
+    @objc private func didAboutClick(_ sneder: Any) {
+        present(AboutMeViewController(), animated: true, completion: nil)
+    }
+
+    @objc private func didLogClick(_ sender: Any) {
+        present(UINavigationController(rootViewController: LogController()), animated: true, completion: nil)
     }
     
 }
